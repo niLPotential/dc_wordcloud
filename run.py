2badbd97ba47bcadb0a71edbf67e2d53a959a1cb
Dongjun Park <pdj5096@gmail.com>
2019-06-09 22:13:21 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
-import requests, lxml
+import requests, lxml, os
 from wordcloud import WordCloud
 
-fontpath='C:/Windows/Fonts/NotoSansMonoCJKkr-Regular.otf'
+fontpath='font.otf'
 tdata = ''
+ndata = ''
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
@@ -13,7 +14,6 @@ headers = {
 name = input('갤러리 입력:')
 keyword = input('검색어 입력(*=전체):')
 page = int(input('조회 페이지 범위 입력(최소 1 이상):'))
-dtype = input('수집 항목 [title:제목(기본값), nick:닉네임]:')
 
 print('갤러리 검색중...')
 r = requests.get('https://search.dcinside.com/gallery/q/' + name, headers = headers).text
@@ -33,43 +33,42 @@ if 'integrate_cont_list' in r:
         print('마이너')
     else:
         print('정식')
-
-    print('값 읽어내는 중...')
-
+        
     for i in range(1, page + 1):
 
-        print(i,'번째 페이지 읽는 중...')
-        r = requests.get(link + '&page=' + str(i), headers = headers).text
+        print('페이지 읽는 중... [{}/{}]'.format(i, page), end='\r')
         
+        r = requests.get(link + '&page=' + str(i), headers = headers).text    
         bs = BeautifulSoup(r, 'lxml')
 
-        if dtype == 'nick':
-            tmp1 = bs.find_all('td', class_='gall_tit ub-word')
-            tmp2 = bs.find_all("td", {"class", "gall_writer ub-writer"})
+        tmp1 = bs.find_all('td', class_='gall_tit ub-word')
+        tmp2 = bs.find_all("td", {"class", "gall_writer ub-writer"})
 
-            post_data = zip(tmp1, tmp2)
-            
-            for s in post_data:
-                 if str(s[0]).find('<b>')==-1 and s[1]['data-nick'].strip() != 'ㅇㅇ': tdata += s[1]['data-nick'].strip() + '\n'
-        else:
-            tmp1 = bs.find_all('td', class_='gall_tit ub-word')
-            tmp2 = bs.find_all('td', class_='gall_num')
+        post_data = zip(tmp1, tmp2)
+        
+        for s in post_data:
+             if str(s[0]).find('<b>')==-1 and s[1]['data-nick'].strip() != 'ㅇㅇ': ndata += s[1]['data-nick'].strip() + '\n'
 
-            post_data = zip(tmp1, tmp2)
+        tmp2 = bs.find_all('td', class_='gall_num')
 
-            for s in post_data:
-                if str(s[0]).find('<b>')==-1 and ((keyword in s[0].find('a').text) or keyword=='*'):
-                    #print(s[1].text + '\t' + s[0].find('a').text)
-                    tdata += s[0].find('a').text + '\n'
+        post_data = zip(tmp1, tmp2)
 
-    #open('result.txt', 'w').write(tdata)
-    print('워드클라우드 생성 중...')
-    wordcloud = WordCloud(font_path=fontpath, width=1280, height=720, background_color='white').generate(tdata)
+        for s in post_data:
+            if str(s[0]).find('<b>')==-1 and ((keyword in s[0].find('a').text) or keyword=='*'):
+                tdata += s[0].find('a').text + '\n'
 
-    plt.figure(figsize=(12,12))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    plt.show()
+    print()
+    print('워드클라우드 생성 중... [1/2]', end='\r')
+    wc_title = WordCloud(font_path=fontpath, width=1920, height=1080, background_color='white').generate(tdata)
+    
+    print('워드클라우드 생성 중... [2/2]')
+    wc_nick = WordCloud(font_path=fontpath, width=1920, height=1080, background_color='white').generate(ndata)
+
+    print('이미지 저장 중...')
+    wc_title.to_file('title.png')
+    wc_nick.to_file('nick.png')
+
+    print('저장 완료')
     
 else:
     print('해당 명칭의 갤러리가 없습니다.')
